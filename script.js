399cd1bda9a91765dc2686d244753e2da3434fdb
Taras Korabel <marina@taras-latitude-e7240.lan>
2022-05-19 22:14:26 +0200

    fixed broken links in Script.js for random background images

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 console.log ("you can see, Js is linked.");
 
 function dropdown () {
- 
+
   var height = document.getElementById('info');
   var dropdown = document.getElementById('dropdown');
-  
+
   if (dropdown.style.color === "gray") {
     info.style.display = "none";
     dropdown.style.color = "black";
   } else {
-    
+
     dropdown.style.color = "gray";
     info.style.display = "block";
   }
@@ -17,12 +17,12 @@ function dropdown () {
 function home () {
     var height = document.getElementById('welcome');
     var dropdown = document.getElementById('homebutton');
-  
+
   if (dropdown.style.color === "gray") {
     welcome.style.display = "none";
     homebutton.style.color = "black";
   } else {
-    
+
     homebutton.style.color = "gray";
     welcome.style.display = "block";
   }
@@ -34,24 +34,24 @@ function about () {
     about.style.display = "none";
     aboutbutton.style.color = "black";
   } else {
-    
+
     aboutbutton.style.color = "gray";
     about.style.display = "block";
   }
 }
 
 function wallpaper () {
-    
-    var image1 = "url(/images/neonwall.png)";
-    var image2 = "url(/images/gradientdisplay.png)";
-    var image3 = "url(/images/trianglify.jpg)";
+
+    var image1 = "url(neonwall.png)";
+    var image2 = "url(gradientdisplay.png)";
+    var image3 = "url(trianglify.jpg)";
     var image4 = "url('/images/neon_cube.png')";
     var image5 = "url('https://cdna.artstation.com/p/assets/images/images/048/734/668/medium/taras-k-nuclear-submarine-improved.jpg?1650811235')";
     var image6 = "url('https://cdna.artstation.com/p/assets/images/images/040/643/348/medium/taras-k-procedural-wood-material-preview.jpg?1629462691')";
     var image7 = "url('https://cdnb.artstation.com/p/assets/images/images/040/643/359/large/taras-k-r.jpg?1629462704')";
-    var image8 = "url(/images/PlexusNeo2.png)";
-    var image9 = "url(/images/gradientdisplay3.png)";
-    var image10 = "url(/images/gradientdisplay4.png)";
+    var image8 = "url(PlexusNeo2.png)";
+    var image9 = "url(gradientdisplay3.png)";
+    var image10 = "url(gradientdisplay4.png)";
     var array1 = new Array(image1, image2, image3, image4, image5, image6, image7, image8, image9, image10);
     var randomItem = array1[Math.floor(Math.random()*array1.length)];
     document.getElementById('body').style.backgroundImage = randomItem;
@@ -71,8 +71,8 @@ function blackpage () {
 function collapse () {
     document.getElementById("navbar").style.backgroundImage = "none" ;
     document.getElementById("navbar").style.backgroundColor = "rbga('10,10,10,02')";
-   
+
 }
 function scale () {
-    
+
 }s
