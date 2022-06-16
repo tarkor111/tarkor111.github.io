9d06de2ed9abdfea1c73c69c016d0a1b1e137db4
tarkor111 <marina@taras-latitude-e7240.lan>
2022-06-16 08:46:30 +0200

    Navbar is now shrinked on scroll!

--- script.js
+++ script.js
@@ -79,3 +79,15 @@ function scale () {
 function copymail () {
   navigator.clipboard.writeText("tarkor111@gmail.com");
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "50px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.height = "100px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
